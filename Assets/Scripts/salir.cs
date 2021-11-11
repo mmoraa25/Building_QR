56f5577da266210f84921e9194349be82db18c7a
mmoraa25 <mlmoraa25@gmail.com>
2021-11-10 22:21:22 -0500

    Commit Inicial

---
+++ Assets/Scripts/salir.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salir : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SalirApp() {
+        Application.Quit();
+
+    }
+}
